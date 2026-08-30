c7718a8c9f4009e5e70168911f83bd6161cbf4f6
Habeeb.Wasif <83237316+Habeebwasif@users.noreply.github.com>
2026-08-30 17:42:15 +0530

    Added disk_check.sh

---
+++ 2026/day-18/Scripts/disk_check.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+check_disk() {
+    echo "Disk Usage:"
+    df -h /
+}
+
+check_memory() {
+    echo "Memory Usage:"
+    free -h
+}
+
+# Main section
+check_disk
+
+echo "---------------------"
+
+check_memory
